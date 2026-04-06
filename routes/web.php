34eba5aed725d4d28315308a2fb0f42dc13f7cb0
zaraa-kuki <azfara2006@gmail.com>
2026-04-06 09:55:54 +0700

    Add route for Dashboard page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// 1. Panggil Controller-nya di sini
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 2. Tambahin route buat ke halaman Dashboard
+Route::get('/dashboard', [DashboardController.php, 'index']);
