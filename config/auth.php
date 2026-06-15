0513ac562524fa517c8f3da4bfc4c8921b91bb23
zaraa-kuki <azfara2006@gmail.com>
2026-06-15 09:08:53 +0700

    Resolve merge conflicts in auth.php configuration

--- config/auth.php
+++ config/auth.php
@@ -40,13 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-<<<<<<< HEAD
         'admin' => [
             'driver' => 'session',
             'provider' => 'users', // Kita pake provider yang sama karena lu bilang tabelnya sama
         ],
-=======
->>>>>>> e81a9824da7f0bcb2b495d2eebe5295f4f295424
     ],
 
     /*
@@ -72,17 +69,10 @@ return [
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
-<<<<<<< HEAD
         'admins' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class, // Tetap arahin ke User model, tapi nanti di logic Controller kita pastiin role-nya admin
         ],
-=======
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
->>>>>>> e81a9824da7f0bcb2b495d2eebe5295f4f295424
     ],
 
     /*
@@ -111,7 +101,6 @@ return [
             'expire' => 60,
             'throttle' => 60,
         ],
-<<<<<<< HEAD
 
         'admins' => [
             'provider' => 'users', // pake provider users aja karena tabel admin & user lu sama
@@ -119,8 +108,6 @@ return [
             'expire' => 60,
             'throttle' => 60,
         ],
-=======
->>>>>>> e81a9824da7f0bcb2b495d2eebe5295f4f295424
     ],
 
     /*
@@ -133,10 +120,4 @@ return [
     | confirmation screen. By default, the timeout lasts for three hours.
     |
     */
-<<<<<<< HEAD
-=======
-
-    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
-
->>>>>>> e81a9824da7f0bcb2b495d2eebe5295f4f295424
 ];
